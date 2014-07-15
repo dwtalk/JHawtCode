d696f5617081d1c8146f3a29081572e08c3e1ee0
Dustin Talk <dwtalk@gmail.com>
2014-07-15 14:36:54 -0500

    updated analytics

--- src/main/java/com/ddubyat/develop/jhawtcode/web/InternalResourceController.java
+++ src/main/java/com/ddubyat/develop/jhawtcode/web/InternalResourceController.java
@@ -9,11 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.UUID;
+import java.security.*;
+import java.net.NetworkInterface;
 
 @Controller
 public class InternalResourceController {
+
+    String systemUUID = "";
 
     @Autowired
     private ResourceUtil resourceUtil;
@@ -51,7 +60,7 @@ public class InternalResourceController {
 
     @ResponseBody
     @RequestMapping(value = "/jhawtcode/jhc.js", method = {RequestMethod.GET})
-    public byte[] requestJS(HttpServletResponse response) throws IOException {
+    public byte[] requestJS(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setHeader("Content-Encoding", "gzip");
         response.setHeader("Server", "jhawtconsole");
         response.setContentType("application/javascript");
@@ -63,8 +72,52 @@ public class InternalResourceController {
             return (new String("")).getBytes();
         }
 
+        if(systemUUID.equalsIgnoreCase("")) {
+            try {
+                //generate from hardware
+                Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+                while (nis.hasMoreElements()) {
+                    NetworkInterface ni = nis.nextElement();
+                    systemUUID += Arrays.toString(ni.getHardwareAddress());
+                }
+            } catch (Exception e) {
+                //generate a uuid randomly for failures
+                systemUUID = UUID.randomUUID().toString();
+            }
+            //md5 the uuid
+            try {
+                byte[] bytesOfMessage = systemUUID.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                byte[] thedigest = md.digest(bytesOfMessage);
+                systemUUID = new BigInteger(1, thedigest).toString(16);
+            } catch (Exception e) {
+                systemUUID = "1234567890";
+            }
+        }
+
+        String appname = "noappname";
+        if(request.getSession() != null && request.getSession().getServletContext() != null && request.getSession().getServletContext().getServletContextName() != null) {
+            appname = request.getServletContext().getServletContextName();
+        }
+
+        String username = "nouser";
+        if(System.getProperty("user.name") != null && StringUtil.isNotEmpty(System.getProperty("user.name"))) {
+            username = System.getProperty("user.name");
+        }
+
+        String license = "demo";
+        if(System.getProperty("jhawtcode.license") != null && StringUtil.isNotEmpty(System.getProperty("jhawtcode.license"))) {
+            license = System.getProperty("jhawtcode.license");
+        }
+
+        String javascript = resourceUtil.readLocalResource(new String[]{"classpath:/js/jhawtcode.js"});
+        javascript = javascript.replace("|systemUUID|", systemUUID);
+        javascript = javascript.replace("|username|", username);
+        javascript = javascript.replace("|license|", license);
+        javascript = javascript.replace("|appname|", appname);
+
         try {
-            return resourceUtil.gzipCompress(resourceUtil.readLocalResource(new String[]{"classpath:/js/jhawtcode.js"}));
+            return resourceUtil.gzipCompress(javascript);
         } catch (IOException ioe) {
             return ioe.getLocalizedMessage().getBytes("UTF-8");
         }
